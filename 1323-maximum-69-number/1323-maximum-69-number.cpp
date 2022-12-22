813ad5a67d086db5621186b49fe2c4e669d87fc6
Harsh <92624180+hq1233@users.noreply.github.com>
2022-12-22 20:07:07 +0530

    Time: 3 ms (38.79%), Space: 6 MB (46.37%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-               string s=to_string(num);//converting int to string 
+               string s=to_string(num);
         for(int i=0;i<s.size();++i)
         {
             if(s[i]=='6')
