bdcb17659ebbc78705db490254c77c8ca7a3e838
Harsh <92624180+hq1233@users.noreply.github.com>
2022-12-28 00:19:35 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (25.32%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> arr;
+
+        while(n != 1 && arr.find(n) == arr.end()){
+            arr.insert(n);
+            int c = 0;
+            while(n){
+                c += (n%10) * (n%10);
+                n /= 10;
+            }
+            n = c;
+        }
+        return n == 1;
+    }
+};
