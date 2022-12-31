efaa2f48320a000c77d974a1b7a6c42d8af3cede
Harsh <92624180+hq1233@users.noreply.github.com>
2022-12-31 19:33:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.80%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1=1,prev2=1;
+        for(int i=2;i<=n;i++){
+            int curi=prev1+prev2;
+            prev2=prev1;
+            prev1=curi;
+        }
+        return prev1;
+    }
+};
